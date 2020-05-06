ea5c450deb7c8bf3c2f498d70fdbacf9a55c557e
vinod-tak <64468701+vinod-tak@users.noreply.github.com>
2020-05-06 18:53:40 +0530

    first upload

---
+++ 1.py
@@ -0,0 +1,15 @@
+txt=input("enter a character:")
+
+if("a" == txt):
+    print(txt,"is a vowel");
+
+elif("e" == txt):
+    print(txt,"is a vowel");
+elif("i" == txt):
+    print(txt,"is a vowel");
+elif("o" == txt):
+    print(txt,"is a vowel");
+elif("u" == txt):
+    print(txt,"is a vowel");
+else:
+    print(txt,"is a consonant");
