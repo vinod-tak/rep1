c2f09446a94be1469808ae9bb7899e934cc05924
vinod-tak <64468701+vinod-tak@users.noreply.github.com>
2020-05-12 18:51:12 +0530

    fifth upload

---
+++ 2.py
@@ -0,0 +1,24 @@
+a=int(input("enter a number"))
+b =int(input("enter a number:"))
+d =int(input("enter a number:"))
+'''for i in range(1,21):
+    print(a,'x',i,'=',a*i);
+    
+if a>0:
+    print("positive")
+elif a==0:
+    print("zero")
+else:
+print("negative")'''
+if a>b:
+    if a>d:
+        print(a,"is greater");
+elif b>a:
+    if b>d:
+        print(b,"id greater");
+elif d>a:
+    if d>b:
+        print(d,"is greater");
+        
+    
+
